41a88f4276a1a8a4326097c0fe1b8a9c9b63967a
Navin Pai <navinpai_kma@yahoo.com>
2011-11-23 03:43:00 +0530

    Updated download.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="Stay offline!" />
+
+<title> 8Tracks Playlist Downloader</title>
+<link rel="stylesheet" href="style/style.css" type="text/css"/>
+</head>
+<body>
+<div id="page">
+<div id="body">
+<div id="header">
+ <img src="style/header.jpg" border="0" align="centre"/>
+ </div>
+<form id="form" action="download.php" class="bold_form signup_form" method="post">
+PLAYLIST URL<br/> 
+<span class="field">
+<input type="text" id="playlist" name="playlist" />
+</span>
+<p/>
+<p class="submit">
+<input type="submit" class="button alt_button" value="DOWNLOAD" />
+</p>
+</form>
+</div>
+</div>
+
+</body>
+</html> 
